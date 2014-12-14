fe9cf6a2fbd4c5ff2450369fd3fd47c37848c295
vitkarpov <viktor.s.karpov@gmail.com>
2014-12-14 12:25:44 +0300

    Generated documentation for b937d82fae0476283c71e5cb3ab4e007c4bdb259

---
+++ toc.js
@@ -0,0 +1,49 @@
+window.files = [
+{
+  "path": "index.html",
+  "originalName": "README.md",
+  "originalPath": "README.md",
+  "name": "index.html",
+  "title": "jQuery.BEM: helps to work with BEM entities",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "jquery-bem-helps-to-work-with-bem-entities",
+      "title": "jQuery.BEM: helps to work with BEM entities"
+    },
+    {
+      "level": 2,
+      "slug": "api",
+      "title": "API"
+    },
+    {
+      "level": 3,
+      "slug": "setmod",
+      "title": "setMod"
+    },
+    {
+      "level": 2,
+      "slug": "delmod",
+      "title": "delMod"
+    },
+    {
+      "level": 2,
+      "slug": "hasmod",
+      "title": "hasMod"
+    },
+    {
+      "level": 2,
+      "slug": "togglemod",
+      "title": "toggleMod"
+    }
+  ]
+},
+{
+  "path": "jquery.bem.js.html",
+  "originalName": "jquery.bem.js",
+  "originalPath": "jquery.bem.js",
+  "name": "jquery.bem.js.html",
+  "lang": "javascript",
+  "toc": []
+}
+];
